4c956c59dbf50533120643eb4b767ef97ab6fb41
Shahab uddin <114737338+shahab3051@users.noreply.github.com>
2026-07-30 17:35:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
 const CONFIG = {
 
   // Supabase project URL
-  SUPABASE_URL: "https://hpqheouhcxsvikkwelnm.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://hpqheouhcxsvikkwelnm.supabase.co",
 
   // Supabase Publishable / Anon key
-  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwcWhlb3VoY3hzdmlra3dlbG5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MzE5NTIsImV4cCI6MjEwMTAwNzk1Mn0.jdExYItPtENJpk9wdlqZ2_2aWP82BTq5GyyzymfdXZs",
+  SUPABASE_KEY: "sb_publishable_p0kO7rqSrQpxbhWKo1V_4w_SZyD-6RS",
 
   // Supabase table name
   SUPABASE_TABLE: "Refund_data",
